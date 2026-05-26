db145ba32e18244ea1074786cc106618594c6528
Olivia Nicolle <olivia.nicolle.72@gmail.com>
2026-05-26 10:25:18 -0300

    Add files via upload

--- proj-01.js
+++ proj-01.js
@@ -1,51 +1,48 @@
-let ListaPessoas = "Ana-15,Bruno-20,Carla-25,Diego-30";
-let nomes = ListaPessoas.split(",");
 let pessoas = [];
 
 function adicionar() {
     let nome = document.getElementById("nome").value;
     let idade = parseInt(document.getElementById("idade").value);
 
-    if ( nome == "" || idade < 0) {
+    
+    if (nome === "" || isNaN(idade) || idade < 0) {
         alert("Por favor, preencha os campos corretamente.");
         return;
     }
   
-pessoas.push({ nome: nome, idade: idade });
+    pessoas.push({ nome: nome, idade: idade });
 
-let item = document.createElement("li");
-item.textContent = nome + " - " + idade + " anos";
-document.getElementById("lista").appendChild(item);
+    let item = document.createElement("li");
+    item.textContent = nome + " - " + idade + " anos";
+    document.getElementById("lista").appendChild(item);
 
-document.getElementById("nome").value = "";
-document.getElementById("idade").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
 }
+
 function exibirNomes() {
-    nomes.forEach(function(nome) {
-        console.log("forEach1: " + nome);
+  
+    pessoas.forEach(function(pessoa) {
+        console.log("forEach1: " + pessoa.nome);
     });
 
-    nomes.forEach(function(nome) {
-        console.log("forEach2: " + nome);
+    pessoas.forEach(function(pessoa) {
+        console.log("forEach2: " + pessoa.nome);
     });
 
-    for (let nome of nomes) {
-        console.log("for let: " + nome);
+    for (let pessoa of pessoas) {
+        console.log("for let: " + pessoa.nome);
     }
 }
 
-function Analisar() {
-   pessoas.sort(function(a, b) {
+function analisar() {
+   
+    pessoas.sort((a, b) => {
+        if (a.nome !== b.nome) {
+            return a.nome.localeCompare(b.nome);
+        }
         return a.idade - b.idade;
     });
-
-let lista= document.getElementById("lista");
-lista.innerHTML = "";
-
-for (let i = 0; i < nomes.length; i++) {
-    let item = document.createElement("li");
-   item.innerText = pessoas[i].nome + " - " + pessoas[i].idade + " anos";
-    lista.appendChild(item);
+    
+    console.log("Lista ordenada:", pessoas);
 }
-}
-
