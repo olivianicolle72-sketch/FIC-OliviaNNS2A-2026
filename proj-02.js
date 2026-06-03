d96c64ff2adc95960c9d1bbfe305350107e349bc
Olivia Nicolle <olivia.nicolle.72@gmail.com>
2026-06-03 07:53:17 -0300

    Add files via upload

--- proj-02.js
+++ proj-02.js
@@ -1,20 +1,21 @@
-function calcular() {
-            let num1 = parseFloat(document.getElementById("num1").value);
-            let num2 = parseFloat(document.getElementById("num2").value);
-            let operacao = document.getElementById("operacao").value;
+ 
+        function calcular() {
+            let num1 = document.getElementById("num1").value;
+            let num2 = document.getElementById("num2").value;
+            let operacao = document.getElementById("operacao");
             let resultado;
 
-            if (isNaN(num1) || isNaN(num2)) {
-                resultado = "Ops! Digite os números";
-            } else {
-                switch (operacao) {
-                    case "soma": resultado = num1 + num2; break;
-                    case "subtracao": resultado = num1 - num2; break;
-                    case "multiplicacao": resultado = num1 * num2; break;
-                    case "divisao": 
-                        resultado = num2 !== 0 ? (num1 / num2).toFixed(2) : "Não pode dividir por 0! "; 
-                        break;
-                }
+            if(isNaN (num1) || isNaN (num2)){
+                resultado = "Digite corretamente";
             }
-            document.getElementById("resultado").textContent = "Resultado: " + resultado;
+            else{
+              switch(operacao){
+                case "soma": resultado = num1 + num2; break;
+                case "subtracao": resultado = num1 - num2; break;
+                case "multiplicacao": resultado = num1*num2; break;
+                case "divisao":
+                    resultado: num2 ==! 0 ? (num1/num2): "Não pode dividir por 0!"; 
+              }
+              document.getElementById("resultado") = "Resultado: " + resultado;
         }
+    }
